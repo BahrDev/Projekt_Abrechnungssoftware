61eb8fe6a44bc5609a60d1dced04293862dc0a6f
Stefan Bahr <Bahr.Javadev@gmail.com>
2020-06-23 12:04:28 +0200

    viele Methodengerüste eingefügt

--- src/package_Abrechnungssoftware/TabRechnung.java
+++ src/package_Abrechnungssoftware/TabRechnung.java
@@ -1,5 +1,38 @@
 package package_Abrechnungssoftware;
 
-public class TabRechnung {
+import java.util.ArrayList;
+import java.util.Date;
 
+public class TabRechnung {
+	
+	// Attribute
+	
+	private String betreff;
+	private String anrede;
+	private String anschreiben;
+	private ArrayList<Rechnungsposten> rechnungspositionen = new ArrayList<Rechnungsposten>();
+	private double summeNetto;
+	private double rechnungsbetrag;
+	private Date rechnungsDatum;
+	
+	// Konstruktoren
+	
+	// Methoden
+	public void rechnungspostenHinzufügen() {
+		
+	}
+	
+	public void rechnungspostenEntfernen() {
+		
+	}
+	
+	public void speichern() {
+		
+	}
+	
+	public void drucken() {
+		
+	}
+	
+	// Getter/Setter
 }
--- src/package_Abrechnungssoftware/TabRechnungsposition.java
+++ src/package_Abrechnungssoftware/TabRechnungsposition.java
@@ -1,5 +1,49 @@
 package package_Abrechnungssoftware;
 
-public class TabRechnungsposition {
+import java.util.ArrayList;
 
+public class TabRechnungsposition {
+	
+	// Attribute
+	private String positionsname;
+	private ArrayList<String> kategorie = new ArrayList<String>();
+	private double satzEuro;
+	private String satzArt;
+	
+	// Konstruktoren
+	
+	// Methoden
+	
+	public void positionAnzeigen() {
+		
+	}
+
+	public void positionAnlegen() {
+		
+	}
+	
+	public void kategorieAnlegen() {
+		
+	}
+	
+	public void speichern() {
+		
+	}
+	
+	public void eingabefelderLeeren() {
+		
+	}
+	
+	public boolean checkAufUngesicherteDaten() {
+		// Methode schreiben!
+		return false;
+	}
+	
+	public void frageZurAbsicherung() {
+		//JOptionPane hier
+	}
+	
+	// Getter/Setter
+	
+	
 }

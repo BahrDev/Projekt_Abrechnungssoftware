ffdffa5bb02ccdeae832df0eebdbb75361fa9676
Stefan Bahr <Bahr.Javadev@gmail.com>
2020-06-23 10:58:11 +0200

    Suchleiste
    Attribute und Klassengerüste hinzugefügt

--- src/package_Abrechnungssoftware/Suchleiste.java
+++ src/package_Abrechnungssoftware/Suchleiste.java
@@ -1,5 +1,34 @@
 package package_Abrechnungssoftware;
 
-public class Suchleiste {
+import java.util.ArrayList;
 
+public class Suchleiste {
+	
+	// Attribute
+	private ArrayList<String> rechnungspositionen = new ArrayList<String>();
+
+	private ArrayList<String> kunden = new ArrayList<String>();
+	
+	private ArrayList<String> tempListSuche = new ArrayList<String>();
+	
+	private String tempSuchWort;
+	
+	// Konstruktoren
+	
+	
+	// Methoden
+	
+	public void generiereArrayLists() {
+		
+	}
+	
+	public ArrayList implementiereAutofill() {
+		return tempListSuche;
+	}
+	
+	public ArrayList sortiereListe(ArrayList<String> unsortiert) {
+		ArrayList<String> sortiert = unsortiert;
+		return sortiert;
+	}
+	
 }

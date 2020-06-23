9b2c1e51fa0d96b939c15e42339176eeb5c87f3a
Patrick, Herzog <parickherzog@gmail.com>
2020-06-23 12:04:53 +0200

    Klassen bearbeitet
    Methoden und Attribute aus Klassendiagramm übertragen

--- src/package_Abrechnungssoftware/Druckauftrag.java
+++ src/package_Abrechnungssoftware/Druckauftrag.java
@@ -1,5 +1,18 @@
 package package_Abrechnungssoftware;
 
 public class Druckauftrag {
-
+	
+	//Methode
+	
+	public String findeDrucker()
+	{
+		String druckername = null;
+		return druckername;
+	}
+	
+	public void druckePDF()
+	{
+		//Methode schreiben
+	}
+	
 }
--- src/package_Abrechnungssoftware/TabKunde.java
+++ src/package_Abrechnungssoftware/TabKunde.java
@@ -1,5 +1,77 @@
 package package_Abrechnungssoftware;
 
+import java.util.ArrayList;
+
+import javax.swing.JOptionPane;
+
 public class TabKunde {
 
+	// Attribute 
+	private String kundenName; 
+	private String kundeID; 
+	private String strasseID;
+	private String ort;
+	private int plz;
+	private String telefonNr;
+	private String email;
+	private String steuerNr;
+	private ArrayList<String> rechnungen = new ArrayList<String>();
+	
+	// gegebenenfalls weitere Strings von Programmtext
+	
+	
+	//Methoden
+	public void kundeAnzeigen() 
+	{
+		
+	}
+	
+	public void kundeAnlegen() 
+	{
+		
+	}
+	
+	public void speichern() 
+	{
+		
+	}
+	
+	public void rechnungAnzeigen() 
+	{
+		
+	}
+	
+	public void rechnungKorrigieren()
+	{
+		
+	}
+	
+	public void rechnungStellen()
+	{
+		
+	}
+	
+	public ArrayList<String> ladeRechnungen()
+	{
+	// Methode schreiben 
+		return rechnungen;	
+	}
+	
+	public void eingabefelderLeeren()
+	{
+		
+	}
+	//Abfrage boolean 
+	public boolean checkAufUngesicherteDaten()
+	{
+	//Methode schreiben
+		return false;
+	}
+	
+	public void frageZurAbsicherung()
+	{
+	//Methode schreiben
+	JOptionPane sind_sie_sicher = new JOptionPane();	
+	}
+	
 }

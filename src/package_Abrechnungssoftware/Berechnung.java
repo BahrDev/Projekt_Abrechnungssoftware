d9c720ef0325fdadf03b329d60f1aed76f8ceacb
Patrick Herzog <parickherzog@gmail.com>
2020-07-08 09:25:36 +0200

    Berechnung v0.1
    Methoden zur Berechnung erstellt
    DateiGenerierung ergänzt

--- src/package_Abrechnungssoftware/Berechnung.java
+++ src/package_Abrechnungssoftware/Berechnung.java
@@ -5,17 +5,40 @@ import java.util.ArrayList;
 public class Berechnung {
 
 	
-	public double berechneRechnungspositionsSumme(double Wert1,double Wert2) 
-	{
-		//Methode schreiben
-		double Summe = Wert1 + Wert2;
+	public void berechneRechnungspositionsSumme() {
 		
-		return Summe;
-	}
-	public double berechneGesamtSumme(ArrayList<Rechnungsposten> rechnungspositionen)
-	{
 		//Methode schreiben
-		double Gesamtsumme = 1;
-		return Gesamtsumme;
+		ArrayList<Rechnungsposten> rechnungsposten = GUI.getTk1().getNeueGUIRechnung().getTr1().getAktuelleRechnung().getRechnungsposten();
+		double summe;
+		double satz_Euro;
+		int einheiten;
+		
+		for (int i = 0; i < rechnungsposten.size() ; i++) {
+			
+			satz_Euro = rechnungsposten.get(i).getRechnungspostenSatzEuro();
+			einheiten = rechnungsposten.get(i).getRechnungspostenEinheiten(); // Wert wird noch in int geändert
+			summe = satz_Euro * einheiten;
+			rechnungsposten.get(i).setRechnungspostenGesamtEuro(summe);
+			
+		}
+		
+	}
+	public void berechneGesamtSumme(ArrayList<Rechnungsposten> rechnungspositionen) {
+		//Methode schreiben
+		
+		ArrayList<Rechnungsposten> rechnungsposten = GUI.getTk1().getNeueGUIRechnung().getTr1().getAktuelleRechnung().getRechnungsposten();
+		
+		double gesamt_Euro = 0.0;
+		
+		for (int i = 0; i < rechnungsposten.size(); i++) {
+			
+			gesamt_Euro += rechnungsposten.get(i).getRechnungspostenGesamtEuro();
+			GUI.getTk1().getNeueGUIRechnung().getTr1().getAktuelleRechnung().setRechnungSummeNetto(gesamt_Euro);
+			GUI.getTk1().getNeueGUIRechnung().getTr1().getAktuelleRechnung().setRechnungEndbetrag(gesamt_Euro);
+		
+			
+		}
+		
+		
 	}
 }
